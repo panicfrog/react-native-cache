b17ae6604f3bb5c83ed16d16adc295c3988fc44e
yeyeongping <yongping.ye@za.group>
2022-07-15 15:19:54 +0800

    add ios build script

---
+++ cpp/ios_build.sh
@@ -0,0 +1,81 @@
+rm -rf output 
+rm -rf build
+rm -rf tem
+
+mkdir -p build/devices
+mkdir -p build/simulator
+
+sims=("SIMULATOR" "SIMULATOR64" "SIMULATORARM64")
+
+for SIM in ${sims[@]}
+do 
+
+P="build/simulator/${SIM}"
+
+mkdir -p "${P}"
+
+done
+
+mkdir tem && cd tem
+
+cmake .. -G Xcode \
+-DCMAKE_TOOLCHAIN_FILE=../../thirdparty/ios-cmake/ios.toolchain.cmake \
+-DPLATFORM=OS \
+-DDEPLOYMENT_TARGET=10.0
+
+cmake --build . --config Release
+
+cp Release-iphoneos/* ../build/devices/
+cp SQLiteCppOutDir/Release-iphoneos/* ../build/devices/
+cp SQLiteCppOutDir/sqlite3/Release-iphoneos/* ../build/devices/
+
+
+for SIM in ${sims[@]}
+do 
+
+P="../build/simulator/${SIM}"
+
+rm -rf ./*
+
+cmake .. -G Xcode \
+-DCMAKE_TOOLCHAIN_FILE=../../thirdparty/ios-cmake/ios.toolchain.cmake \
+-DPLATFORM=${SIM} \
+-DDEPLOYMENT_TARGET=10.0
+
+cmake --build . --config Release
+
+cp Release-iphonesimulator/* ${P}/
+cp SQLiteCppOutDir/Release-iphonesimulator/* ${P}/
+cp SQLiteCppOutDir/sqlite3/Release-iphonesimulator/* ${P}/
+
+done
+
+libs=("libHCache.a" "libSQLiteCpp.a" "libsqlite3.a")
+
+for LIB in ${libs[@]} 
+do 
+lipo -create ../build/simulator/SIMULATOR/${LIB} ../build/simulator/SIMULATOR64/${LIB} ../build/simulator/SIMULATORARM64/${LIB} -output ../build/simulator/${LIB}
+done
+
+for SIM in ${sims[@]}
+do
+rm -rf ../build/simulator/${SIM}
+done
+
+#merge static library
+mkdir -p merge/simulator
+mkdir -p merge/devices
+
+libtool -static simulator/*.a -o merge/simulator/libHCache.a
+libtool -static devices/*.a -o merge/devices/libHCache.a
+
+xcodebuild -create-xcframework \
+  -library merge/simulator/libHCache.a \
+  -library merge/devices/libHCache.a \
+  -output HCache.xcframework
+
+# clean 
+rm -rf devices
+rm -rf merge
+rm -rf simulator
+
